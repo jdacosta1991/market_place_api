9f0aa8d80494b3c75fa8aa96c1d26b7c58040bd9
jdacosta1991 <workdacojo@gmail.com>
2022-05-05 09:11:17 -0400

    add versionsih

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   #api definicion
   namespace :api, defaults: { format: :json } do
-    #lista de recursos
+    namespace :v1 do
+      #lista de recursos
+      
+    end
   end
 end
